ed07dec950b79318f181b0aae43f303ac63cba82
System Administrator <admin@example.com>
2020-05-11 13:59:55 +0000

    Changes by 'System Administrator' on 'TestSnow2 - 1.0.0' -- Build #1

---
+++ sn/TestSnow2/Script Include/active_true/x_226232_testsnow2.TestScript.js
@@ -0,0 +1,28 @@
+/* 
+ * Application : TestSnow2
+ * ClassName   : sys_script_include
+ * Created On  : 2020-05-11 13:03:08
+ * Created By  : admin
+ * Updated On  : 2020-05-11 13:03:18
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=bd4db4101bb890104e06fc86cc4bcbe7
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_226232_testsnow2.module:sys_script_include
+ */
+var TestScript = Class.create();
+TestScript.prototype = /** @lends x_226232_testsnow2.module:sys_script_include.TestScript.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+    type: 'TestScript'
+};
